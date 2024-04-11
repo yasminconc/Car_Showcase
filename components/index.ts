7ea07e678317a93f7c45b83c21a5d415415f1437
yasminconc <yasminsilvaconc@gmail.com>
2024-04-11 12:27:08 -0300

    .

---
+++ components/index.ts
@@ -0,0 +1,12 @@
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from './Navbar'
+import Footer from './Footer'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacture from './SearchManufacturer'
+import CarCard from './CarCard'
+import { CarDetails } from "./CarDetails";
+
+export { Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacture, CarCard, CarDetails };
